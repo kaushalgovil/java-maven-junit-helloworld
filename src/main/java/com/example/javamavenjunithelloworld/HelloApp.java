83945c8871f1eaf6e8f5588f0cbd5714d8aa3541
kaushal <balliatic@gmail.com>
2019-01-03 11:33:19 +0000

    added text 5

--- src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -1,7 +1,7 @@
 package com.example.javamavenjunithelloworld;
 
 /**
- *Test change 4
+ *Test change 5
  * A very basic program that demonstrates the use of JUnit tests. The tests include a sample unit test and an
  * integration test.
  */
