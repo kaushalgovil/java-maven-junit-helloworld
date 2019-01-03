a8ef170c6f250bddc9809a25a85b9e8f25f469c2
kaushal <balliatic@gmail.com>
2019-01-03 13:38:13 +0000

    added text 11

--- src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -1,7 +1,7 @@
 package com.example.javamavenjunithelloworld;
 
 /**
- *Test change 10
+ *Test change 11
 
  * A very basic program that demonstrates the use of JUnit tests. The tests include a sample unit test and an
  * integration test.
